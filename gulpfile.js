64d8a859bb102e94a3fe0a1c343b5d33da9ac981
Paweł Błach <paul.blach@gmail.com>
2019-02-05 14:34:32 +0100

    🥤 Added Gulp

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const watchify = require('watchify');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const log = require('gulplog');
+
+const customOpts = {
+    entries: ['./client/src/index.js'],
+    debug: true
+  };
+const opts = Object.assign({}, watchify.args, customOpts);
+const b = watchify(browserify(opts));
+
+gulp.task('js', bundle); // so you can run `gulp js` to build the file
+b.on('update', bundle); // on any dep update, runs the bundler
+b.on('log', log.info); // output build logs to terminal
+
+function bundle() {
+    return b.bundle()
+        .on('error', log.error.bind(log, 'Browserify Error'))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./client/static/js'));
+}
